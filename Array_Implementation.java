15bd90d77d6090c13cab19594697f767a7723553
utkarsh2603 <utkarsh70888@gmail.com>
2023-01-31 09:42:10 +0530

    Initial Commit

--- Array_Implementation.java
+++ Array_Implementation.java
@@ -1,2 +1,27 @@
-public class Array_Implementation {
+public class Array_Implementation
+{
+    public static void main(String[] args) {
+        //array creation of size 10
+        int array[]=new int[10];
+        //array insertion
+        for(int i=1;i<array.length;i++)
+        {
+            array[i]=i*2;
+        }
+        //printing the inserted values in array
+        for(int i=1;i<array.length;i++)
+        {
+            System.out.print(array[i]+" ");
+        }
+        System.out.println();
+        //updating values
+        array[4]=1000;
+
+        for(int i=1;i<array.length;i++)
+        {
+            System.out.print(array[i]+" ");
+        }
+
+
+    }
 }
